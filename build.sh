4b4115988041788dcc20cff924abd14d4e70fa92
luna brown <silicasandwhich@gmail.com>
2020-11-02 19:52:54 -0500

    fixed build script

--- build.sh
+++ build.sh
@@ -71,7 +71,7 @@ done
     echo -e "\033[0KGenerating pngs of silicon carbide... DONE"
 
     echo -ne "\033[0KGenerating animated cursors for silicon carbide... \\r"
-for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
 do
     echo -ne "\033[0KGenerating pngs for silicon carbide Progress... $i / 45 \\r"
 
@@ -90,7 +90,7 @@ do
 done
     echo -e "\033[0KGenerating pngs for silicon carbide Progress... DONE"
 
-for i in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
 do
     echo -ne "\033[0KGenerating pngs for silicon carbide Wait... $i / 45 \\r"
 
