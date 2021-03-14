e973e6925af56509ebbe2b860686c407b8e49146
silicasandwich <silicasandwhich@gmail.com>
2021-03-14 15:48:38 -0400

    pngs

--- tools/svg-to-png.bat
+++ tools/svg-to-png.bat
@@ -1,1 +1,1 @@
-FOR %%A IN (24,28,32,40,48,56,64,72,80,88,96) DO inkscape -w %%A -h %%A -p svgs\%1.svg -o pngs\%%A\%1.png
+FOR %%A IN (32) DO inkscape -w %%A -h %%A -p svgs\%1.svg -o pngs\%%A\%1.png
